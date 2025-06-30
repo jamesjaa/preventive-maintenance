453770bca83fae42ae685c2f072953455f1dbe67
JamesDevNoi <144088647+jamesjaa@users.noreply.github.com>
2025-07-01 01:59:01 +0700

    1/7/68
    Success

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MaintenanceSchedule;
+use App\Models\EqmLog;
+use Carbon\Carbon;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('layouts.dashboard');
+        $maintenance = MaintenanceSchedule::query()
+            ->leftJoin('equipment', 'maintenance_schedule.hw_id', '=', 'equipment.hw_id')
+            ->leftJoin('groups', 'equipment.groups_id', '=', 'groups.groups_id')
+            ->leftJoin('type', 'equipment.type_id', '=', 'type.type_id')
+            ->leftJoin('brand', 'equipment.brand_id', '=', 'brand.brand_id')
+            ->leftJoin('model', 'equipment.model_id', '=', 'model.model_id')
+            ->leftJoin('zone', 'equipment.zone_id', '=', 'zone.zone_id')
+            ->select(
+                'maintenance_schedule.*',
+                'equipment.hw_sn',
+                'equipment.hw_name',
+                'groups.*',
+                'type.type_name',
+                'brand.brand_name',
+                'model.model_name',
+                'zone.zone_name'
+            )
+            ->whereDate('maintenance_schedule.planned_date', '<=', Carbon::today())
+            ->where('status', 1)
+            ->get();
+
+        $query = EqmLog::query();
+        $totalCost = $query->sum('cost');
+
+        return view('layouts.dashboard', ['maintenance' => $maintenance, 'totalCost' => $totalCost]);
     }
 }
