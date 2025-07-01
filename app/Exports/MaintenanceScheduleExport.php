9c121fd34d76d9af1da5d2e07ca5aec6bfd0c130
JamesDevNoi <144088647+jamesjaa@users.noreply.github.com>
2025-07-02 01:04:37 +0700

    2//7/86
    update export excel

--- app/Exports/MaintenanceScheduleExport.php
+++ app/Exports/MaintenanceScheduleExport.php
@@ -2,10 +2,13 @@
 
 namespace App\Exports;
 
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class MaintenanceScheduleExport implements FromView
+class MaintenanceScheduleExport implements FromCollection, WithHeadings, WithStyles, WithColumnWidths
 {
     protected $data;
 
@@ -14,10 +17,60 @@ class MaintenanceScheduleExport implements FromView
         $this->data = $data;
     }
 
-    public function view(): View
+    public function collection()
     {
-        return view('export.maintenance_schedule_excel', [
-            'data' => $this->data
-        ]);
+        // แปลง collection ให้เป็น array ของ row
+        return $this->data->map(function ($item, $key) {
+            return [
+                $key + 1,
+                $item->groups_name,
+                $item->type_name,
+                $item->hw_sn,
+                $item->brand_name,
+                $item->model_name,
+                $item->hw_name,
+                \Carbon\Carbon::parse($item->planned_date)->format('d/m/Y'),
+            ];
+        });
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'กลุ่มอุปกรณ์',
+            'ชนิด',
+            'SN',
+            'แบรนด์',
+            'โมเดล',
+            'ชื่ออุปกรณ์',
+            'PM Plan Date',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // ทำหัวตารางหนา
+        $sheet->getStyle('A1:H1')->getFont()->setBold(true);
+        // กำหนด background color
+        $sheet->getStyle('A1:H1')->getFill()
+            ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('FFE4E4E4');
+        // จัดกึ่งกลางทุกคอลัมน์
+        $sheet->getStyle('A:H')->getAlignment()->setHorizontal('center');
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 5,
+            'B' => 20,
+            'C' => 15,
+            'D' => 20,
+            'E' => 15,
+            'F' => 15,
+            'G' => 25,
+            'H' => 15,
+        ];
     }
 }
--- app/Exports/PMReportExport.php
+++ app/Exports/PMReportExport.php
@@ -2,10 +2,13 @@
 
 namespace App\Exports;
 
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class PMReportExport implements FromView
+class PMReportExport implements FromCollection, WithHeadings, WithStyles, WithColumnWidths
 {
     protected $logs;
 
@@ -14,10 +17,64 @@ class PMReportExport implements FromView
         $this->logs = $logs;
     }
 
-    public function view(): View
+    public function collection()
     {
-        return view('export.report_excel', [
-            'logs' => $this->logs
-        ]);
+        return $this->logs->map(function ($log, $index) {
+            return [
+                $index + 1,
+                \Carbon\Carbon::parse($log->planned_date)->format('d/m/Y'),
+                \Carbon\Carbon::parse($log->actual_date)->format('d/m/Y'),
+                $log->groups_name,
+                $log->type_name,
+                $log->hw_sn,
+                $log->brand_name,
+                $log->model_name,
+                $log->hw_name,
+                number_format($log->cost, 2)
+            ];
+        });
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'PM Plan Date',
+            'วันที่ PM อุปกรณ์',
+            'กลุ่มอุปกรณ์',
+            'ชนิดอุปกรณ์',
+            'SN',
+            'แบรนด์',
+            'โมเดล',
+            'ชื่อของอุปกรณ์',
+            'ค่าใช้จ่าย'
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // หัวตารางหนา + สีพื้น
+        $sheet->getStyle('A1:J1')->getFont()->setBold(true);
+        $sheet->getStyle('A1:J1')->getFill()
+            ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+            ->getStartColor()->setARGB('FFE4E4E4');
+        // จัดกึ่งกลางทุกคอลัมน์
+        $sheet->getStyle('A:J')->getAlignment()->setHorizontal('center');
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 5,
+            'B' => 15,
+            'C' => 15,
+            'D' => 20,
+            'E' => 20,
+            'F' => 20,
+            'G' => 15,
+            'H' => 15,
+            'I' => 25,
+            'J' => 12,
+        ];
     }
 }
